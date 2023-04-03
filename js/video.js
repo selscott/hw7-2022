217a9a38ee923f30f3de961941bb44fab9fc69e2
selscott <72165902+selscott@users.noreply.github.com>
2023-04-02 22:37:11 -0400

    update video.js w/ value modifications

--- js/video.js
+++ js/video.js
@@ -21,17 +21,17 @@ document.querySelector("#pause").addEventListener("click", function() {
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
-	video.playbackRate *= 0.95
+	video.playbackRate *= 0.90
 	console.log("Speed is " + video.playbackRate)
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	video.playbackRate /= 0.95
+	video.playbackRate /= 0.90
 	console.log("Speed is " + video.playbackRate)
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
-	video.currentTime += 15
+	video.currentTime += 10
 	
 	if (video.currentTime >= video.duration) {
 		video.currentTime = 0
